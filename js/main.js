2ec71f3756f134361e0be3dd352ad67f5fb6e1af
Mhamudul_Islam <141499999+Mhamudulislam@users.noreply.github.com>
2024-01-02 00:51:26 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,113 @@
+var countdown =  $("#countdown").countdown360({
+radius      : 60,
+seconds     : 30,
+fontColor   : '#FFFFFF',
+autostart   : false,
+onComplete  : function () { console.log('done') }
+});
+countdown.start();
+console.log('countdown360 ',countdown);
+$(document).on("click","button",function(e){
+e.preventDefault();
+var type = $(this).attr("data-type");
+if(type === "time-remaining")
+{
+var timeRemaining = countdown.getTimeRemaining();
+alert(timeRemaining);
+}
+else
+{
+var timeElapsed = countdown.getElapsedTime();
+alert(timeElapsed);
+}
+});
+
+
+
+
+
+
+// 2
+var countdown =  $("#countdown2").countdown360({
+    radius      : 60,
+    seconds     : 30,
+    fontColor   : '#FFFFFF',
+    autostart   : false,
+    onComplete  : function () { console.log('done') }
+    });
+    countdown.start();
+    console.log('countdown360 ',countdown);
+    $(document).on("click","button",function(e){
+    e.preventDefault();
+    var type = $(this).attr("data-type");
+    if(type === "time-remaining")
+    {
+    var timeRemaining = countdown.getTimeRemaining();
+    alert(timeRemaining);
+    }
+    else
+    {
+    var timeElapsed = countdown.getElapsedTime();
+    alert(timeElapsed);
+    }
+    });
+
+
+
+
+
+
+    // 3
+var countdown =  $("#countdown3").countdown360({
+    radius      : 60,
+    seconds     : 30,
+    fontColor   : '#FFFFFF',
+    autostart   : false,
+    onComplete  : function () { console.log('done') }
+    });
+    countdown.start();
+    console.log('countdown360 ',countdown);
+    $(document).on("click","button",function(e){
+    e.preventDefault();
+    var type = $(this).attr("data-type");
+    if(type === "time-remaining")
+    {
+    var timeRemaining = countdown.getTimeRemaining();
+    alert(timeRemaining);
+    }
+    else
+    {
+    var timeElapsed = countdown.getElapsedTime();
+    alert(timeElapsed);
+    }
+    });
+
+
+
+
+
+
+        // 4
+var countdown =  $("#countdown4").countdown360({
+    radius      : 60,
+    seconds     : 30,
+    fontColor   : '#FFFFFF',
+    autostart   : false,
+    onComplete  : function () { console.log('done') }
+    });
+    countdown.start();
+    console.log('countdown360 ',countdown);
+    $(document).on("click","button",function(e){
+    e.preventDefault();
+    var type = $(this).attr("data-type");
+    if(type === "time-remaining")
+    {
+    var timeRemaining = countdown.getTimeRemaining();
+    alert(timeRemaining);
+    }
+    else
+    {
+    var timeElapsed = countdown.getElapsedTime();
+    alert(timeElapsed);
+    }
+    });
